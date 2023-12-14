3265ee8bbac191565ea92b1b1656672541a9b02a
shivani2825 <jainshivani285@gmail.com>
2023-12-14 12:18:06 +0530

    ChatGpt Integration

---
+++ chatgptinte.py
@@ -0,0 +1,21 @@
+
+from openai import OpenAI
+
+client = OpenAI(api_key = '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')
+
+user_prompt = input("Enter your prompt: ")
+
+response = client.chat.completions.create(
+  model="gpt-3.5-turbo-1106",
+  response_format={ "type": "json_object" },
+  messages=[
+    {"role": "system", "content": "You are a helpful assistant designed to output JSON."},
+    {"role": "user", "content": user_prompt}
+  ]
+)
+print(response.choices[0].message.content)
+
+
+
+
+
